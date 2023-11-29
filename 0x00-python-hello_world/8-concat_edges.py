88316d84a8068cc71765d73117c049fb24a55937
Yeabtakele <berekatabe434@gmail.com>
2023-11-29 11:39:31 -0800

    good

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming with Python \
- language that combines remarkable power with very clear syntax" 
-print(str[str.find("object-oriented programming with Python"):
-str.find("object-oriented programming with Python") + 26])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
