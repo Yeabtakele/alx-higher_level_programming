b2d80f0f6ab55185b84df754cdc5b9d8369a4037
Yeabtakele <berekatabe434@gmail.com>
2023-12-03 07:04:17 -0800

    w

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,16 +2,10 @@
 # -----------------------------------------------------------
 # Python program that:
 # demonstrates how to print all intger
-# -----------------------------------------------------------
+# ----------------------------------------------------------
 
+#!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """Prints all intergers of a list
-
-    Args:
-      my_list: the list argument whose items will be printed
-    """
-
-    # Iterate through list
-    for integer in my_list:
-        print("{:d}".format(integer))
+    for i in my_list:
+        print("{:d}".format(i))
