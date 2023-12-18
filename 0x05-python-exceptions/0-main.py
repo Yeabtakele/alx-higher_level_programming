8bc115b7fb6d04054262efa3d695406fff0c0fde
yeabtakele <bereketabe434@gmail.com>
2023-12-19 00:23:42 +0300

    f

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -8,7 +8,8 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))#!/usr/bin/python3
+print("nb_print: {:d}".format(nb_print))
+#!/usr/bin/python3
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
@@ -19,3 +20,4 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
-        try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
-            break
-    print()
-    return n
+        count = 0
+
+            try:
+                        for i in my_list:
+                                        if count < x:
+                                                            print(i, end="")
+                                                                            count += 1
+                                                                                except:
+                                                                                            pass
+                                                                                            finally:
+                                                                                                        print()
+
+                                                                                                            return count
+
+                                                                                                        # Example usage:
+                                                                                                        my_list = [1, 2, 3, 4, 5]
+
+                                                                                                        nb_print = safe_print_list(my_list, 2)
+                                                                                                        print("nb_print: {:d}".format(nb_print))
+                                                                                                        nb_print = safe_print_list(my_list, len(my_list))
+                                                                                                        print("nb_print: {:d}".format(nb_print))
+                                                                                                        nb_print = safe_print_list(my_list, len(my_list) + 2)
+                                                                                                        print("nb_print: {:d}".format(nb_print))
+
