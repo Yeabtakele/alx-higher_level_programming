76032bac59033a3c506839ad9bd67eee9e582d84
yeabtakele <bereketabe434@gmail.com>
2023-12-19 00:31:16 +0300

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-        count = 0
+    """Print x elememts of a list.
 
-            try:
-                        for i in my_list:
-                                        if count < x:
-                                                            print(i, end="")
-                                                                            count += 1
-                                                                                except:
-                                                                                            pass
-                                                                                            finally:
-                                                                                                        print()
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
 
-                                                                                                            return count
-
-                                                                                                        # Example usage:
-                                                                                                        my_list = [1, 2, 3, 4, 5]
-
-                                                                                                        nb_print = safe_print_list(my_list, 2)
-                                                                                                        print("nb_print: {:d}".format(nb_print))
-                                                                                                        nb_print = safe_print_list(my_list, len(my_list))
-                                                                                                        print("nb_print: {:d}".format(nb_print))
-                                                                                                        nb_print = safe_print_list(my_list, len(my_list) + 2)
-                                                                                                        print("nb_print: {:d}".format(nb_print))
-
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
