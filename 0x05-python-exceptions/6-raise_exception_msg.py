f030f78ef8b261b2414ca10ae7fc569b0132ae91
yeabtakele <bereketabe434@gmail.com>
2023-12-18 23:51:00 +0300

    did C is fun

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+ # Raise a NameError with the provided message
+def raise_exception_msg(message=""):
+                                                     
+            raise NameError(message)
+
+                                                 
+                
+                                                                          
+                              
+                                                   
+            
