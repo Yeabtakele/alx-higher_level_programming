f1257de5bbd6f259c049aaf2c8538e5e273087c4
Yeabtakele <bereketabe434@gmail.com>
2024-02-15 07:22:38 -0800

    fixed

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
--- Write a script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS `%%DATABASE_NAME%%`.`first_table` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+-- Creates a table called first_table with values id and name.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
