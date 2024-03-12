d5f179522f49d65a321f82d5998595a4db35db36
Yeabtakele <bereketabe434@gmail.com>
2024-03-12 09:30:49 -0700

    fixed

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
 class Rectangle {
-	  constructor(width, height) {
-		      this.width = width;
-		      this.height = height;
-		    }
 
-	  getArea() {
-		      return this.width * this.height;
-		    }
-
-	  getPerimeter() {
-		      return 2 * (this.width + this.height);
-		    }
 }
-
 module.exports = Rectangle;
