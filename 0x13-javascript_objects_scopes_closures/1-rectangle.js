01d3c7f8d33411926a1790067aef932df50b92d6
yeabtakele <bereketabe434@gmail.com>
2024-03-12 20:07:07 +0300

    task rectangle 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//  empty class Rectangle that defines a rectangle
+//empty class Rectangle that defines a rectangle
    class Rectangle {
 	     constructor (w, h) {
 		         this.width = w;
