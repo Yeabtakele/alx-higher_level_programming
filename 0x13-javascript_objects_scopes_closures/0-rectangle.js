41904e6733d091a52cb4cbabaee3ea6b1719c7ae
yeabtakele <bereketabe434@gmail.com>
2024-03-12 19:49:12 +0300

    task rectangle.js is fixed

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 // Write an empty class Rectangle that defines a rectangle
 class Rectangle {
-	  constructor(width, height) {
-		      this.width = width;
-		      this.height = height;
-		    }
-
-	  getArea() {
-		      return this.width * this.height;
-		    }
-
-	  getPerimeter() {
-		      return 2 * (this.width + this.height);
-		    }
 }
 
 module.exports = Rectangle;
