b93992372dbca4caa73ffb18add004f0a1d95785
Yeabtakele <bereketabe434@gmail.com>
2024-03-13 00:17:43 -0700

    Sorted occurences 12 challenge

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,31 +1,24 @@
 #!/usr/bin/node
-const MyDict = require('./101-data.js').dict;
+// Import the dictionary from 101-data.js
+// Create an empty dictionary for user IDs by occurrence
+// Loop through each key-value pair in the original dictionary
+const dict = require('./101-data.js').dict;
 const NewDict = {};
 const list = [];
 let MyList = [];
 let Newlist = [];
 
-for (const i in MyDict) {
-  const X = MyDict[i];
-  // console.log(i);
-  // console.log(X);
-  list.push(X);
-}
-
-// console.log(list);
-MyList = [...new Set(list)];
-// console.log(MyList);
-MyList.sort();
-const y = MyList.length;
-for (let x = 0; x < y; x++) {
-  Newlist = [];
-  // console.log(MyList[x]);
-  for (const j in MyDict) {
-    if (MyList[x] === MyDict[j]) {
-      Newlist.push(j);
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
     }
   }
-  NewDict[MyList[x]] = Newlist;
-// console.log(Newlist);
+  newDict[valsUniq[j]] = list;
 }
-console.log(NewDict);
+console.log(newDict);
