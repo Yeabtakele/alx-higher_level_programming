6bea9e75cf586235d4a51ffabe8a58b70485e0f8
Yeabtakele <bereketabe434@gmail.com>
2024-04-02 18:53:24 +0300

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "You got me!" user_id=98" 0.0.0.0:5000/catch_me
+ curl -sX PUT -L -d "user_id=98" --header "origin: X-School-User-Id " 0.0.0.0:5000/catch_me
