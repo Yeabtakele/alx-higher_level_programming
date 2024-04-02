4af77ee71846aca887bf7d328340a8b4afe7c693
Yeabtakele <bereketabe434@gmail.com>
2024-04-02 17:40:23 +0300

    reponse to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a GET request to the URL
-curl -sL "$1"
+# Write a Bash script that takes in a URL, for 200 status code responses.
+  curl -sL "$1"
