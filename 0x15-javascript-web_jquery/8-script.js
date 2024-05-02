56c293141fb68aca439d028dd2a40b4a308dd955
yeabtakele <bereketabe434@gmail.com>
2024-05-03 02:10:50 +0300

    the 8-query

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data){
+     ('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+		  });
+	 });
